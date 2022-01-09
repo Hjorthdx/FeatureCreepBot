3d7842287f3f459b18cd3e6c06367e04099ba979
Hjorth <sboels18@student.aau.dk>
2022-01-09 12:03:18 +0100

    dotenv now works.

--- src/client.ts
+++ src/client.ts
@@ -1,36 +1,37 @@
-import path from "path";
-import { Intents, Interaction, Message } from "discord.js";
-import { Client } from "discordx";
-import { importx } from "@discordx/importer";
+import path from 'path';
+import { Intents, Interaction } from 'discord.js';
+import { Client } from 'discordx';
+import { importx } from '@discordx/importer';
+import config from './config';
 
 const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    Intents.FLAGS.GUILD_VOICE_STATES,
-  ],
-  botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
-  silent: false,
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.GUILD_VOICE_STATES,
+	],
+	botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
+	silent: false,
 });
 
 client.once('ready', async () => {
-  await client.initApplicationCommands({
-    guild: { log: true },
-    global: { log: true },
-  });
-  await client.initApplicationPermissions();
+	await client.initApplicationCommands({
+		guild: { log: true },
+		global: { log: true },
+	});
+	await client.initApplicationPermissions();
 
-  console.log('Client initialized');
+	console.log('Client initialized');
 });
 
 client.on('interactionCreate', (interaction: Interaction) => {
-  client.executeInteraction(interaction);
+	client.executeInteraction(interaction);
 });
 
 // Doesn't work I think
-client.on('unhandledRejection', error => console.error('Promise rejection:', error));
+client.on('unhandledRejection', (error) => console.error('Promise rejection:', error));
 
 importx(path.join(__dirname, 'commands', '**/*.{ts,js}')).then(() => {
-  client.login(''); // Remember to reinsert it here when this doesn't work =)
+	client.login(config.botToken);
 });
